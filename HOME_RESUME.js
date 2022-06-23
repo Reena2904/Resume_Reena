cd1c39b49cc4f12ab2cf6f6d780ac09ce6ba56fc
Reena2904 <108048043+Reena2904@users.noreply.github.com>
2022-06-23 13:42:18 +0530

    Add files via upload

---
+++ HOME_RESUME.js
@@ -0,0 +1,33 @@
+
+    function myFunction1() {
+      var x1= document.getElementById("myDIV1");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function myFunction2() {
+      var x2 = document.getElementById("myDIV2");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function myFunction3() {
+      var x3 = document.getElementById("myDIV3");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function myFunction4() {
+      var x4 = document.getElementById("myDIV4");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
